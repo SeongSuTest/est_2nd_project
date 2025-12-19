1850eefc8c5c14a77b60c7420ed82dcf339aa27b
SeongSuTest <143207975+SeongSuTest@users.noreply.github.com>
2025-12-19 10:14:03 +0900

    fix: container max-width 속성 수정

--- js/best.js
+++ js/best.js
@@ -4,39 +4,4 @@ import { renderFooter } from "./modules/footer.js";
 document.addEventListener("DOMContentLoaded", () => {
   renderHeader();
   renderFooter();
-
-  const ul = document.querySelector("header ul");
-
-  function updateMenuClass() {
-    if (window.innerWidth >= 1200) {
-      ul.classList.add("menu-tt");
-      ul.classList.remove("mobileMenu-tt");
-    } else {
-      ul.classList.add("mobileMenu-tt");
-      ul.classList.remove("menu-tt");
-    }
-  }
-  updateMenuClass();
-  window.addEventListener("resize", updateMenuClass);
-  const menu = document.querySelector("header ul");
-
-  menu.addEventListener("click", (e) => {
-    const button = e.target.closest("button");
-    if (!button) return;
-
-    menu.querySelectorAll("button.active").forEach((btn) => {
-      btn.classList.remove("active");
-    });
-
-    button.classList.add("active");
-  });
-
-  const header = document.querySelector("header");
-  const menuBtn = document.querySelector(".mobile-menu-button");
-
-  menuBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    menuBtn.classList.toggle("active");
-    header.classList.toggle("is-menu-open");
-  });
 });
