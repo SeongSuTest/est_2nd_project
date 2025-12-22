00eff12f32ca8c568eb2ddd28d6d4553588da4e1
KimDuHyun-web <duhyeon467@gmail.com>
2025-12-22 18:08:20 +0900

    feat: index 수정

--- js/main.js
+++ js/main.js
@@ -17,46 +17,70 @@ const swiper = new Swiper('.swiper', {
   slidesPerView: 3,
   spaceBetween: 16,
   breakpoints: {
-    320: {slidesPerView: 2},
-    480: {slidesPerView: 3},
-    640: {slidesPerView: 4},
+    480: {slidesPerView: 2},
+    768: {slidesPerView: 3},
   },
-  
 });
 
 const slideWrap = document.querySelector('.slideItemsWrapper');
 const slideItems = slideWrap.querySelector('.slideItems');
+const glassEl = document.querySelector('#glassList');
+const sunglassEl = document.querySelector('#sunglassList');
+const saleEl = document.querySelector('#saleList');
 
-function createSlideItem(d){
-  const price = d.price.toLocaleString();
+let listData = [];
 
-  return `
-  <li class="swiper-slide">
+fetch('/data/main-slide-item.json')
+  .then(res=>res.json())
+  .then(result=>{
+    const products = result.mainProducts;
+    console.log(result);
+
+    renderList(
+      products.filter(p => p.category === 'glass'),
+      glassEl
+    );
+    renderList(
+      products.filter(p => p.category === 'sunglass'),
+      sunglassEl
+    );
+    renderList(
+      products.filter(p => p.category === 'sale'),
+      saleEl
+    );
+  })
+
+function renderList(data, target){
+  let HTML = '';
+
+  data.forEach(p=>{
+  const price = p.price.toLocaleString();
+
+    HTML +=
+    `<li class="swiper-slide">
       <a href="#">
         <div class="thumb">
-          <img src="${d.image}" alt="${d.title}">
+          <img src="${p.image}" alt="${p.title}">
         </div>
         <dl class="glassDesc">
-          <dt class="desc-tt">${d.brand}</dt>
-          <dd class="body-tt">${d.title}</dd>
+          <dt class="desc-tt">${p.brand}</dt>
+          <dd class="body-tt">${p.title}</dd>
           <dd>
             ${
-              d.discountPercent > 0
-                ? `<span class="h4-tt">${d.discountPercent}%</span>`
+              p.discountPercent > 0
+                ? `<span class="h4-tt">${p.discountPercent}%</span>`
                 : ``
             }
             <p class="h4-tt">${price}원</p>
           </dd>
         </dl>
       </a>
-    </li>
-  `;
+    </li>`;
+  });
+  target.innerHTML = HTML;
 }
 
-fetch('/data/main-slide-item.json')
-  .then(res=>{
-    return res.json()
-    .then(data=>{
-      renderGlassSlide(data.mainProducts);
-    });
-  });
+
+
+  
+  /* 슬라이드 끝 */
